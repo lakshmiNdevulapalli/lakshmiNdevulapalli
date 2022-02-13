9ddc12ac8679c7a159e55595fe2c2df93b9d47d5
Lakshmi Narasimha Balasubrahmanyam <baludevulapalli@gmail.com>
2022-02-13 22:42:54 +0000

    smallest window problem

---
+++ Practice_python/smallestWindow.py
@@ -0,0 +1,25 @@
+#Given an array of integers that are out of order 
+# input: [3,7,5,6,9]
+# output: (1, 3)
+
+def window(array):
+  left, right = None, None 
+  n = len(array)
+  max_seen, min_seen = -float("inf"), float("int")
+
+  for i in range(n):
+    max_seen = max(max_seen, array[i])
+    if array[i] < max_seen:
+      right = i
+  
+  for i in range(n - 1, -1, -1):
+    min_seen = min(min_seen, array[i])
+    if array[i] > min_seen:
+      left = i 
+  
+  return left, right
+
+
+arr = [3,7,5,6,9]
+res = window(arr)
+print(res)
